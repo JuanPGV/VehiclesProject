d78793c48436c8368ab6f247a2d45dfddcb8bc51
JuanPGV <juangonzv123@gmail.com>
2024-11-14 21:55:00 -0400

    feat: cars struct

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<string>
+#include<limits>
+using namespace std;
+
+struct Car{
+    int id;
+    string model;
+    string brand;
+    string plate;
+    string color;
+    int year;
+    int mileage;
+    string was_rented;
+    string engine;
+    int price;
+    int id_client;
+};
+
+int main(){
+    
+}
